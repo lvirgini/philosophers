6178fc958fe63ffbe3d508f0a3e5c8667e141824
lvirgini <lvirgini@student.42.fr>
2021-09-02 09:53:20 +0200

    correction data race : add get status functions

--- philosopher/srcs/routine/global_routine.c
+++ philosopher/srcs/routine/global_routine.c
@@ -6,7 +6,7 @@
 /*   By: lvirgini <lvirgini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 15:15:27 by lvirgini          #+#    #+#             */
-/*   Updated: 2021/09/01 15:16:03 by lvirgini         ###   ########.fr       */
+/*   Updated: 2021/09/02 09:35:55 by lvirgini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,23 @@ static void	sleeping_routine(t_philo *philo)
 static void	thinking_routine(t_philo *philo)
 {
 	change_status(philo, IS_THINKING);
-	pthread_mutex_unlock(&philo->m_status);
 	print_status(philo, IS_THINKING, philo->rules);
 	usleep(10);
+}
+
+/*
+** "possible data race with valgrind
+** because of read here and write in main thread..."
+*/
+
+static int	get_status(t_philo *philo)
+{
+	int		status;
+
+	pthread_mutex_lock(&philo->m_status);
+	status = philo->status;
+	pthread_mutex_unlock(&philo->m_status);
+	return (status);
 }
 
 /*
@@ -45,21 +59,23 @@ void	*routine(void *philosopher)
 {
 	t_philo		*philo;
 	int			even_odd;
+	int			status;
 
 	philo = (t_philo *)philosopher;
 	even_odd = philo->id % 2;
 	while (can_write(philo->rules))
 	{
-		if (philo->status == IS_THINKING)
+		status = get_status(philo);
+		if (status == IS_THINKING)
 		{
 			if (even_odd == 0)
 				eating_routine_even(philo);
 			else
 				eating_routine_odd(philo);
 		}
-		else if (philo->status == IS_EATING)
+		else if (status == IS_EATING)
 			sleeping_routine(philo);
-		else if (philo->status == IS_SLEEPING)
+		else if (status == IS_SLEEPING)
 			thinking_routine(philo);
 		else
 			return (NULL);
